7d8238ccf64c7f2bd492ed0fc26a214ad367cd4c
Paulo Ricardo <paulo.ricardofsa98@gmail.com>
2024-09-01 16:50:04 -0300

    feat: add search component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import Header from "./_components/header";
+import Search from "./_components/search-input";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="px-5 py-6">
+        <Search />
+      </div>
+    </>
+  );
 }
