db25e5dec9bebde1d9ee9d6f7889bb9de0f8af6b
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2024-09-18 09:30:36 -0300

    refactor: optimize product item component and add restaurant name

--- src/app/_components/product-list.tsx
+++ src/app/_components/product-list.tsx
@@ -8,11 +8,18 @@ const ProductList = async () => {
         gt: 0,
       },
     },
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
     take: 10,
   });
 
   return (
-    <div className="flex overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
+    <div className="flex gap-4 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
       {products.map((product) => (
         <ProductItem key={product.id} product={product} />
       ))}
