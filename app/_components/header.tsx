22fc19e830410d2c986a65b38bcb3ff3997e57ff
Paulo Ricardo <paulo.ricardofsa98@hmail.com>
2024-09-30 17:20:34 -0300

    feat: add initial menu sheet

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,9 +1,31 @@
+"use client";
 import Image from "next/image";
 import { Button } from "./ui/button";
-import { Menu } from "lucide-react";
+import {
+  HomeIcon,
+  LogInIcon,
+  LogOutIcon,
+  Menu,
+  ScrollTextIcon,
+} from "lucide-react";
 import Link from "next/link";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import { Separator } from "./ui/separator";
 
 const Header = () => {
+  const { data } = useSession();
+
+  const handleSingIn = () => signIn();
+  const handleSignOut = () => signOut();
+
   return (
     <div className="flex justify-between px-5 pt-6">
       <Link href={"/"}>
@@ -11,13 +33,92 @@ const Header = () => {
           <Image src="/logo.png" alt="logo" fill className="object-cover" />
         </div>
       </Link>
-      <Button
-        size={"icon"}
-        variant="outline"
-        className="border-none bg-transparent"
-      >
-        <Menu />
-      </Button>
+      <Sheet>
+        <SheetTrigger>
+          <Button
+            size={"icon"}
+            variant="outline"
+            className="border-none bg-transparent"
+          >
+            <Menu />
+          </Button>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle className="text-left">Menu</SheetTitle>
+          </SheetHeader>
+
+          {data?.user ? (
+            <div className="flex justify-between pt-6">
+              <div className="flex items-center gap-3">
+                <Avatar>
+                  <AvatarImage src={data.user?.image as string | undefined} />
+                  <AvatarFallback>
+                    {data.user.name?.split(" ")[0][0]}
+                    {data.user.name?.split(" ")[1][0]}
+                  </AvatarFallback>
+                </Avatar>
+
+                <div>
+                  <h3 className="font-semibold">{data.user.name}</h3>
+                  <span className="block text-xs text-muted-foreground">
+                    {data.user.email}
+                  </span>
+                </div>
+              </div>
+              <Button size={"icon"} onClick={handleSignOut}>
+                <LogOutIcon size={20} />
+              </Button>
+            </div>
+          ) : (
+            <div className="flex items-center justify-between pt-10">
+              <h2 className="font-semibold">Faça seu login</h2>
+              <Button size={"icon"} onClick={handleSingIn}>
+                <LogInIcon />
+              </Button>
+            </div>
+          )}
+
+          <div className="py-6">
+            <Separator />
+          </div>
+
+          <div className="space-y-2">
+            <Button
+              variant={"ghost"}
+              className="w-full justify-start space-x-3 rounded-full text-sm font-normal"
+            >
+              <HomeIcon size={16} />
+              <span className="block">Início</span>
+            </Button>
+
+            {data?.user && (
+              <Button
+                variant={"ghost"}
+                className="w-full justify-start space-x-3 rounded-full text-sm font-normal"
+              >
+                <ScrollTextIcon size={16} />
+                <span className="block">Meus pedidos</span>
+              </Button>
+            )}
+          </div>
+
+          <div className="py-6">
+            <Separator />
+          </div>
+
+          {data?.user && (
+            <Button
+              variant={"ghost"}
+              className="w-full justify-start space-x-3 rounded-full text-sm font-normal"
+              onClick={handleSignOut}
+            >
+              <LogOutIcon size={16} />
+              <span className="block">Sair da conta</span>
+            </Button>
+          )}
+        </SheetContent>
+      </Sheet>
     </div>
   );
 };
